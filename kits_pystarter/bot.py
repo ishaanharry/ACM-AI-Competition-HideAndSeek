fe36fa0627c8de77b0945325d4433504d7449baf
Ishaan Harry Kavoori <ishaanharry@live.com>
2020-06-12 22:31:43 -0700

    initial implementation of greedy algorithm for seekers

--- kits_pystarter/bot.py
+++ kits_pystarter/bot.py
@@ -3,6 +3,24 @@ import math
 import random 
 import sys
 
+def getDistance(x1, y1, x2, y2):
+    return math.sqrt((x2-x1)**2 + (y2-y1)**2)
+
+def greedy(current, target):
+    bestDirection = None
+    closestDistance = math.inf
+
+    for direction in Direction:
+        if(direction==Direction.STILL): continue
+        (x, y) = apply_direction(current.x, current.y, direction.value)
+        distance = getDistance(x, y, target.x, target.y)
+        if (distance < closestDistance):
+            bestDirection = direction
+            closestDistance = distance
+    
+    return bestDirection
+
+
 # Create new agent
 agent = Agent()
 
@@ -28,16 +46,38 @@ while True:
             # game_map[i][j] returns whats on that tile, 0 = empty, 1 = wall, 
             # anything else is then the id of a unit which can be yours or the opponents
 
+            
              # choose a random direction to move in
-            randomDirection = random.choice(list(Direction)).value
+            direction = random.choice(list(Direction)).value
+            
+            
+            if(len(opposingUnits)!=0):
+                closestEnemy = opposingUnits[0]
+                direction = greedy(unit, closestEnemy).value
+            
+            """
+            # choose direction based on greedy algorithm
+            direction = Direction.STILL.value
+            if (len(opposingUnits)==0):
+                direction = random.choice(list(Direction)).value
+            else:
+                
+                closestEnemy = opposingUnits[0]
+                for enemy in opposingUnits:
+                    distance = getDistance(unit.x, unit,y, enemy.x, enemy.y)
+                    if distance == unit.distance:
+                        closestEnemy = enemy
+                
+                direction = greedy(unit, closestEnemy)
+            """
 
             # apply direction to current unit's position to check if that new position is on the game map
-            (x, y) = apply_direction(unit.x, unit.y, randomDirection)
+            (x, y) = apply_direction(unit.x, unit.y, direction)
             if (x < 0 or y < 0 or x >= len(game_map[0]) or y >= len(game_map)):
                 # we do nothing if the new position is not in the map
                 pass
             else:
-                commands.append(unit.move(randomDirection))
+                commands.append(unit.move(direction))
         
     else:
         # AI Code for hider goes here
@@ -55,3 +95,6 @@ while True:
 
     # wait for update from match engine
     agent.update()
+
+
+
